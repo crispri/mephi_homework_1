5b1f3c84fb3edcf7a1c94ea4bc5ba7dc0b735cf8
crispri_g <mary.zackopiryna@yandex.ru>
2022-10-26 23:44:51 +0300

    okk

--- advanced_containers/custom_comparator/task.cpp
+++ advanced_containers/custom_comparator/task.cpp
@@ -1,10 +1,18 @@
 #include "task.h"
 
 bool operator <(const SuperKey& lhs, const SuperKey& rhs) {
+    return lhs.StrPart<=rhs.StrPart;
+
 }
 
 void PopulateMap(
-    std::map<SuperKey, std::string>& map,
-    const std::vector<std::pair<SuperKey, std::string>>& toAdd
+        std::map<SuperKey, std::string>& map,
+        const std::vector<std::pair<SuperKey, std::string>>& toAdd
 ) {
+    for (auto i: toAdd) {
+        map[i.first] = i.second;
+
+    }
 }
+
+
