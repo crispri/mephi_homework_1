f2696b088466c08d567e259ec441b7e7eabd060b
crispri_g <mary.zackopiryna@yandex.ru>
2022-10-26 23:08:16 +0300

    3

--- advanced_containers/list_reverse/task.cpp
+++ advanced_containers/list_reverse/task.cpp
@@ -1,4 +1,11 @@
 #include "task.h"
 
 void ReverseList(std::list<int>& l) {
-}
+    std::list<int>::iterator it;
+
+    std::list<int>::iterator it_=l.end();
+    it_--;
+    for(it=l.begin();it!=l.end();++it){
+        std::swap(*it,*it_);
+        it_--;
+    }
