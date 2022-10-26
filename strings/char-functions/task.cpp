65fd2b2b0cfb7b83e5323d61a516ac4d8e31d8c6
crispri_g <crispri_g@LAPTOP-NMK8BKU6.localdomain>
2022-10-26 19:58:25 +0300

    dead

--- strings/char-functions/task.cpp
+++ strings/char-functions/task.cpp
@@ -1,5 +1,12 @@
+#include "task.h"
 bool isalpha(unsigned char c) {
+    if ((c<='Z' and c>='A') or (c<='z' and c>='a'))
+        return true;
+    return false;
 }
-
 unsigned char tolower(unsigned char c) {
+    if(c<='Z' and c>='A'){
+        c+=32;
+    }
+    return c;
 }
