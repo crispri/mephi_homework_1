0b9d90f2d30de5c38f9a4c5f672a9dd625241263
crispri_g <mary.zackopiryna@yandex.ru>
2022-10-27 21:57:34 +0300

    b

--- advanced_containers/heap_sort/task.cpp
+++ advanced_containers/heap_sort/task.cpp
@@ -1,6 +1,17 @@
 #include "task.h"
-
+#include <vector>
+#include <queue>
 using namespace std;
 
 vector<int> HeapSort(const vector<int>& v) {
+    priority_queue<int>q;
+    for(auto i:v){
+        q.push(i);
+    }
+    vector<int>f(v.size());
+    for(int i=v.size()-1;i>=0;i--){
+        f[i]=q.top();
+        q.pop();
+    }
+    return f;
 }
