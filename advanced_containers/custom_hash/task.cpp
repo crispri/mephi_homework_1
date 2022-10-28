6579f6acdc680b0ee25cd1746a2ed3f5fdc04463
crispri_g <mary.zackopiryna@yandex.ru>
2022-10-28 23:25:30 +0300

    l

--- advanced_containers/custom_hash/task.cpp
+++ advanced_containers/custom_hash/task.cpp
@@ -1,10 +1,10 @@
- #include "task.h"
+#include "task.h"
+using namespace std;
 
 void PopulateHashMap(
-    unordered_map<SuperKey, string>& hashMap,
-    const vector<pair<SuperKey, string>>& toAdd
-) {
-     for(auto i:toAdd){
-         hashMap[i.first]=i.second;
-     }
+        unordered_map<SuperKey, string>& hashMap,
+        const vector<pair<SuperKey, string>>& toAdd) {
+    for (int i = 0; i < (int)toAdd.size(); ++i) {
+        hashMap[toAdd[i].first] = toAdd[i].second;
+    }
 }
